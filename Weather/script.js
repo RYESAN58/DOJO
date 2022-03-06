c2c061c5068c7044dbad20a5d69edfba70acccfb
RYESAN58 <92766524+RYESAN58@users.noreply.github.com>
2022-03-06 03:39:01 -0500

    starting updating process

--- Weather/script.js
+++ Weather/script.js
@@ -11,9 +11,19 @@ function deg(elm){
     var temp = document.querySelectorAll('.hot');
     var temp2 = document.querySelectorAll('.cold');
     for (i =0; i < temp.length; i++){
-        var y = Math.round(temp[i].innerText * 1.8 +32);
-        temp[i].innerText = y;
-        var x = Math.round(temp2[i].innerText * 1.8 +32);
-        temp2[i].innerText = x;
-    }
+        console.log(document.getElementById('dropdown').value)
+        if (document.getElementById('dropdown').value =='f'){
+            var y = Math.round(temp[i].innerText * 1.8 +32);
+            temp[i].innerText = y;
+            var x = Math.round(temp2[i].innerText * 1.8 +32);
+            temp2[i].innerText = x;
+        }
+        else{
+            var z = Math.round((temp[i].innerText - 32)* (5/9));
+            var l = Math.round((temp2[i].innerText - 32)* (5/9));
+            temp[i].innerText = z
+            temp2[i].innerText = l
+        }
+
+        }
 }
